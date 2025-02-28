dc0d31c6885d10dcf901c4acc5a458a87d1c6611
Mrlinkwii <Mrlinkwii@users.noreply.github.com>
2025-02-28 21:09:04 +0000

    d
    yrd

---
+++ pcsx2/LuaEngine.h
@@ -0,0 +1,18 @@
+#ifndef LUA_HEAD
+#define LUA_HEAD
+
+#include "Pcsx2Types.h"
+
+enum LUAExecutionTime
+{
+	SPT_ONCE_ON_LOAD = 0,
+	SPT_CONTINOUSLY = 1,
+
+	_SPT_END_MARKER
+};
+
+extern void ForgetScripts();
+extern void ExecuteScript(LUAExecutionTime Input01);
+extern int LoadScriptFromDir(wxString Input01, const wxDirName &Input02, const wxString &Input03);
+
+#endif
